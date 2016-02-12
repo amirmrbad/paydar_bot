db6e7dbe19b654e957f40b19168cda354685a240
amirmrbad <amirmrbadboy@gmail.com>
2016-02-12 22:22:36 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -246,9 +246,9 @@ function create_config( )
     ‼️ this bot is made by : @creed_is_dead
    〰〰〰〰〰〰〰〰
    ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
+   ߔࠀ   @AMIRmr33
+   ߔࠀ   @shah_paydat
+  
    〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
